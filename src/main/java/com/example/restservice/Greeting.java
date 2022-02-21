9ab2e20e316f59f9df91436fd381641ce374c4db
devesteves <estevesrodri@outlook.com>
2022-02-21 10:37:54 -0300

    Add project lombok

--- src/main/java/com/example/restservice/Greeting.java
+++ src/main/java/com/example/restservice/Greeting.java
@@ -1,20 +1,14 @@
 package com.example.restservice;
+import lombok.Getter;
 
 public class Greeting {
 
-	private final long id;
-	private final String content;
+	@Getter private final long id;
+	@Getter private final String content;
 
 	public Greeting(long id, String content) {
 		this.id = id;
 		this.content = content;
 	}
 
-	public long getId() {
-		return id;
-	}
-
-	public String getContent() {
-		return content;
-	}
 }
